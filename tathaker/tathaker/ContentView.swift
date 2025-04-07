06df2692a09a6edcbb1acf2f84d7c4334ef6c376
alanouda2 <alanoud2@andrew.cmu.edu>
2025-04-07 17:09:15 +0300

    working on demo april7

---
+++ tathaker/tathaker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tathaker
+//
+//  Created by Bullshit  on 09/03/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tathaker/tathakerTests/tathakerTests.swift
@@ -0,0 +1,17 @@
+//
+//  tathakerTests.swift
+//  tathakerTests
+//
+//  Created by Bullshit  on 09/03/2025.
+//
+
+import Testing
+@testable import tathaker
+
+struct tathakerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
