6ba38d98f23fd5500ea728b6abd78cc6e14fea23
alanouda2 <alanoud2@andrew.cmu.edu>
2025-04-08 08:53:13 +0300

    color and extension

---
+++ Tathkerr/Extenstions/Colors.swift
@@ -0,0 +1,13 @@
+//
+//  Colors.swift
+//  Tathaker
+//
+//  Created by Bullshit  on 15/03/2025.
+//
+import SwiftUI
+
+extension Color {
+    static let customDarkBlue = Color(red: 35/255, green: 56/255, blue: 84/255)  // Dark blue
+    static let customLightBlue = Color(red: 214/255, green: 230/255, blue: 242/255) // Light blue
+}
+
